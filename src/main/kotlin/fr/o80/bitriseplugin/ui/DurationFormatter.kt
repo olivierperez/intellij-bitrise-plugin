158d30cfa74368bd35e786ee2f67b70c3094380b
Olivier Perez <olivier.perez@zenika.com>
2023-11-13 13:38:26 +0100

    Fix duration formatting

--- src/main/kotlin/fr/o80/bitriseplugin/ui/DurationFormatter.kt
+++ src/main/kotlin/fr/o80/bitriseplugin/ui/DurationFormatter.kt
@@ -4,6 +4,7 @@ import kotlin.time.Duration
 
 class DurationFormatter {
     fun format(duration: Duration): String = when {
+        duration.inWholeSeconds == 0L -> "0 min"
         duration.inWholeHours < 1 -> duration.formatInMinutes()
         duration.inWholeDays < 1 -> duration.formatInHours()
         duration.inWholeDays < 2 -> duration.formatInDayAndHours()
@@ -12,15 +13,15 @@ class DurationFormatter {
 }
 
 private fun Duration.formatInMinutes(): String {
-    return "$inWholeMinutes min $inWholeSeconds"
+    return "${inWholeMinutes}min ${inWholeSeconds - 60 * inWholeMinutes}s"
 }
 
 private fun Duration.formatInHours(): String {
-    return "${inWholeHours}h ${inWholeMinutes}min"
+    return "${inWholeHours}h ${inWholeMinutes - 60 * inWholeHours}min"
 }
 
 private fun Duration.formatInDayAndHours(): String {
-    return "1 day and $inWholeHours hours"
+    return "1 day and ${inWholeHours - 24 * inWholeDays} hours"
 }
 
 private fun Duration.formatInDays(): String {
