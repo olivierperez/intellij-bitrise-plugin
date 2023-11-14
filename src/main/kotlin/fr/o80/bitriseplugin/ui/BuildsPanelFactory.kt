ee571429fd027cf1880fee9cf01cabde939605b4
Olivier PEREZ <olivier@olivierperez.fr>
2023-11-14 22:58:04 +0100

    Make list scrollable

--- src/main/kotlin/fr/o80/bitriseplugin/ui/BuildsPanelFactory.kt
+++ src/main/kotlin/fr/o80/bitriseplugin/ui/BuildsPanelFactory.kt
@@ -1,6 +1,6 @@
 package fr.o80.bitriseplugin.ui
 
-import com.intellij.ui.components.panels.VerticalLayout
+import com.intellij.ui.components.JBScrollPane
 import fr.o80.bitriseplugin.data.BitriseWebService
 import fr.o80.bitriseplugin.data.HttpClientProvider
 import fr.o80.bitriseplugin.domain.GetBranchBuildsUseCase
@@ -11,6 +11,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import java.awt.BorderLayout
 import java.awt.Component
 import javax.swing.JButton
 import javax.swing.JComponent
@@ -23,25 +24,29 @@ class BuildsPanelFactory {
     private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
     private lateinit var parent: JPanel
-    private var buildsComponent: Component? = null
+    private var buildsComponent: Component = newBranchBuildsList()
 
     fun create(): JComponent {
-        return JPanel(VerticalLayout(0)).apply {
-            add(refreshButton())
-            buildsComponent = add(newBranchBuildsList(listOf()))
+        return JPanel(BorderLayout()).apply {
+            add(refreshButton(), BorderLayout.NORTH)
+            add(buildsComponent, BorderLayout.CENTER)
         }.also {
             parent = it
             refresh()
         }
     }
 
-    private fun newBranchBuildsList(builds: List<Branch>) =
-        VerticalComponent(
-            items = builds,
-            gap = 10
-        ) { build ->
-            BranchBuildsPanel(build)
-                .padding(4)
+    private fun newBranchBuildsList(builds: List<Branch> = emptyList()) =
+        JBScrollPane(
+            VerticalComponent(
+                items = builds
+            ) { build ->
+                BranchBuildsPanel(build)
+                    .padding(horizontal = 4, vertical = 10)
+            }
+        ).apply {
+            verticalScrollBarPolicy = JBScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED
+            horizontalScrollBarPolicy = JBScrollPane.HORIZONTAL_SCROLLBAR_NEVER
         }
 
     private fun refreshButton() = JButton("Refresh").apply {
@@ -49,17 +54,14 @@ class BuildsPanelFactory {
     }
 
     private fun refresh() {
-        buildsComponent?.let {
-            parent.apply {
-                remove(buildsComponent)
-                updateUI()
-            }
-            buildsComponent = null
+        parent.apply {
+            remove(buildsComponent)
+            updateUI()
         }
-
         scope.launch {
             val builds = getBranchBuilds(50)
-            buildsComponent = parent.add(newBranchBuildsList(builds))
+            buildsComponent = newBranchBuildsList(builds)
+            parent.add(buildsComponent, BorderLayout.CENTER)
             parent.updateUI()
         }
     }
